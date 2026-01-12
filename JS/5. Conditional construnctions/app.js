1bfcb2fe2d1869ecc9bff7e1e7a85b11799f4078
sergiuchilat <chilatsergiu@gmail.com>
2026-01-12 16:23:26 +0200

    Conditional constructions

---
+++ JS/5. Conditional construnctions/app.js
@@ -0,0 +1,68 @@
+let x = 1;
+
+if (x > 0) {
+    console.log('x is positive');
+} else {
+    console.log('x is non-positive');
+}
+
+const digit = 5;
+
+if (digit === 0) {
+    console.log('Zero');
+} else if (digit === 1) {
+    console.log('One');
+} else if (digit === 2) {
+    console.log('Two');
+} else if (digit === 3) {
+    console.log('Three');
+} else if (digit === 4) {
+    console.log('Four');
+} else if (digit === 5) {
+    console.log('Five');
+} else if (digit === 6) {
+    console.log('Six');
+} else if (digit === 7) {
+    console.log('Seven');
+} else if (digit === 8) {
+    console.log('Eight');
+} else if (digit === 9) {
+    console.log('Nine');
+} else {
+    console.log('Not a digit');
+}
+
+switch (digit) {
+    case 0:
+        console.log('Zero');
+        break;
+    case 1:
+        console.log('One');
+        break;
+    case 2:
+        console.log('Two');
+        break;
+    case 3:
+        console.log('Three');
+        break;
+    case 4:
+        console.log('Four');
+        break;
+    case 5:
+        console.log('Five');
+        break;
+    case 6:
+        console.log('Six');
+        break;
+    case 7:
+        console.log('Seven');
+        break;
+    case 8:
+        console.log('Eight');
+        break;
+    case 9:
+        console.log('Nine');
+        break;
+    default:
+        console.log('Not a digit');
+}
